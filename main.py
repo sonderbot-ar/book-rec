ae5ddd7c416ca2f597052711ffdd9f4136afd337
sonderbot-ar <sonderbot1@gmail.com>
2026-05-20 23:39:58 +0200

    shitty interface for now.

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
-import pandas as pd
+import gradio as gr 
+from retrieval import get_recommendations
 
-def main():
-    df = pd.read_csv('cleaned_books.csv')
-    print (f'Loaded {len(df)} cleaned book records.')
+def chatbot(message, history):
+    results = get_recommendations(message, top_k=3)
+    response = "Here are some recommendations based on what you asked for:\n"
+
+    for index, row in results.iterrows():
+        response += f"### 📖 **{row['title']}**\n"
+        response += f"**Author:** {row['authors']}\n"
+        response += f"**Genre:** {row['categories']} | **Year:** {int(row['published_year'])}\n"
+        response += f"**Weighted Rating:** {row['weighted_rating']:.2f}/5.0\n"
+        response += '---\n'
+
+    return response
+
+demo = gr.ChatInterface(
+    fn=chatbot,
+    title="Book Recommendation Chatbot",
+    description="Ask for book recommendations based on your preferences!",
+)
 
 if __name__ == "__main__":
-    main()
+    demo.launch()
